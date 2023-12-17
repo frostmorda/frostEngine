bc78e7f2f84070922ea8502a1d890c5e5dc56d31
frostmorda <frost.nsk@.gmail.com>
2023-12-17 12:15:20 +0700

    hot fix

--- src/game/collision.h
+++ src/game/collision.h
@@ -14,7 +14,7 @@ using CollisionInfo = std::tuple<bool, Direction, glm::vec2>;
 class Collision
 {
 public:
-    collision() = delete;
+    Collision() = delete;
     static CollisionInfo CheckBoxCollision(const std::shared_ptr<GameObject> &first, const std::shared_ptr<GameObject> &second);
 
 private:
